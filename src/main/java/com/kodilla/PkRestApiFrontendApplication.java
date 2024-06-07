234457287324f62ec11b1aee01dac5b39bae15d7
Knutek1 <piotr.knuta@gmail.com>
2024-06-07 11:21:48 +0200

    Vaddin z danymi z kursu

---
+++ src/main/java/com/kodilla/PkRestApiFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkRestApiFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkRestApiFrontendApplication.class, args);
+	}
+
+}
